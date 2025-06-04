02d9e71eb124ea8b3f6a8a86381d4d5199cd1aa0
pankaj <rajpal1996kumar@gmail.com>
2025-06-04 12:43:34 +0530

    Section 8 mid code

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,33 +1,78 @@
 const {test, expect } = require('@playwright/test');//we write this to import the package without this we cannot execute our test cases
-test('browser context playwright test', async ({browser})=>{
+test('clint app login', async ({browser})=>{
 
-    const context = await browser.newContext();// to open the new fresh browser
-    const page =await context.newPage();// to create the page on that browser
+      const context = await browser.newContext();
+  const page = await context.newPage();
+ 
 
+   //js file- Login js, DashboardPage
+   
+   
+   await page.goto("https://rahulshettyacademy.com/client");
+   const email = "anshika@gmail.com";
+   const productName = 'ZARA COAT 3';
+   const products = page.locator(".card-body");
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
+   const count = await products.count();
+   for (let i = 0; i < count; ++i) {
+      if (await products.nth(i).locator("b").textContent() === productName) {
+         //add to cart
+         await products.nth(i).locator("text= Add To Cart").click();
+         break;
+      }
+   }
+ 
+   await page.locator("[routerlink*='cart']").click();
+   //await page.pause();
+ 
+   await page.locator("div li").first().waitFor();
+   const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+   expect(bool).toBeTruthy();
+   await page.locator("text=Checkout").click();
+ 
+   await page.locator("[placeholder*='Country']").pressSequentially("ind");
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " India") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+ 
+   await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
+ 
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+ 
+ 
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
-    const username=page.locator('#userEmail');
-    const password=page.locator('#userPassword');
-    const login=page.locator('#login');
-
-
-   await page.goto("https://rahulshettyacademy.com/client")//to open that url on that page
-    console.log(await page.title());// to get the title of that page
-    await username.fill("rapal1996kumgmail.com");
-    await password.fill("POPpopcon22");
-    await login.click();
-
-    await page.waitForLoadState('networkidle');//but if in case this line not works then doo this....
-    // await page.locator(".card-body b").first().waitFor(); // we can use to alter that line also
-
-    const productList = await page.locator(".card-body b").allTextContents();
-    console.log(productList);
-
-    
-
-    
-        
-
+   page.get
+ 
 });
+
 //we can also use the page object model to write the test cases
 
 
